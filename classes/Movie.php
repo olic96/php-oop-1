912715ad62aa0547450b9ee6b98f31fab0c59e18
olic96 <francoolich@gmail.com>
2022-06-27 16:58:41 +0200

    add method

--- classes/Movie.php
+++ classes/Movie.php
@@ -14,5 +14,9 @@
             $this->durata = $_durata;
             $this->regia = $_regia;
             $this->anno = $_anno;
+
+            if($this->regia !== 'Sam Raimi') {
+                $this->titolo = $this->titolo . " " . "(no Raimi, no Spider-Man)";
+            }
         }
     }
